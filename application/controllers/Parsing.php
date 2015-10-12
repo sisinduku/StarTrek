45a43c60c5d042583ab5d11cf5e067ac17753565
Saptanto Sindu <saptanto.sindu@gmail.com>
2015-10-12 23:43:09 +0700

    Parsing Oracle to local DB

--- application/controllers/Parsing.php
+++ application/controllers/Parsing.php
@@ -9,47 +9,41 @@ class Parsing extends CI_Controller {
 	
 	public function parseAutelan() {
 		$this->load->model ( "oracledb" );
-		$result = $this->oracledb->parseDB ();
+		$result = $this->oracledb->parseDBAutelan ();
 		echo $result;
 	}
 	
+	public function parsingOracle(){
+		$this->load->model("oracledb");
+		
+		$result = $this->oracledb->getAP();
+		echo $result;
+	}
 	public function parseUVCisco() {
 		$this->load->model("oracledb");
 		
 		$result = $this->oracledb->getAPUVCisco();
-		$jumlah = array();
-		for($i = 0; $i < 34; $i++)
-			$jumlah[$i] = 0;
-		echo "<table border=1>";
-		foreach ($result as $row){
-			$index = 0;
-			echo "<tr>";
-			foreach ($row as $key => $konten){
-				echo "<td>". $konten. "</td>";
-				if($jumlah[$index] < strlen($konten))
-					$jumlah[$index] = strlen($konten); 
-				$index++;
-			}
-			echo "<td>". $index. "</td>";
-			echo "</tr>";
-		}
-		
-		echo "<tr>";
-		foreach ($jumlah as $data)
-			echo "<td>". $data. "</td>";
-		echo "</tr>";
-		echo "<tr>";
-		for ($i =1; $i <= 34; $i++)
-			echo "<td>". $i. "</td>";
-		echo "</tr>";
-		echo "</table>";
-		print_r(array_keys($result[0]));
+		echo $result;
 	}
 	
 	public function parseUVAutelan() {
 		$this->load->model("oracledb");
 	
 		$result = $this->oracledb->getAPUVAutelan();
-		var_dump($result);
+		echo $result;
+	}
+	
+	public function parseDivreCisco(){
+		$this->load->model("oracledb");
+		
+		$result = $this->oracledb->getgetDivreCisco();
+		echo $result;
+	}
+	
+	public function parseDivreAutelan(){
+		$this->load->model("oracledb");
+	
+		$result = $this->oracledb->getDivreAutelan();
+		echo $result;
 	}
 }
--- application/models/Oracledb.php
+++ application/models/Oracledb.php
@@ -16,7 +16,7 @@ class Oracledb extends CI_Model {
 	 *
 	 * @return number
 	 */
-	public function parseDB() {
+	public function parseDBAutelan() {
 		// Memanggil DB Autelan
 		$this->oracle = $this->load->database ( "oracle", TRUE );
 		$index = 0;
@@ -56,9 +56,27 @@ class Oracledb extends CI_Model {
 		}
 		else{
 			$this->db->empty_table ( 'tbl_autelan' );
-			return "Query Kosong";
+			return "kosong";
 		}
 			
+	}
+	
+	public function getAP(){
+		$result = 0;
+		$this->db->empty_table ( 'tbl_apunverif' );
+		$this->db->empty_table ( 'tbl_apdivre' );
+		$this->db->query("ALTER TABLE tbl_apunverif AUTO_INCREMENT = 1");
+		$this->db->query("ALTER TABLE tbl_apdivre AUTO_INCREMENT = 1");
+		if($this->parseDBAutelan() != "kosong" && $this->getAPUVAutelan() != "kosong" && $this->getAPUVCisco() != "kosong" && $this->getDivreAutelan() != "kosong" && $this->getDivreCisco() != "kosong"){
+			$result += $this->getAPUVAutelan();
+			$result += $this->getAPUVCisco();
+			$result += $this->parseDBAutelan();
+			$result += $this->getDivreAutelan();
+			$result += $this->getDivreCisco();
+		}else{
+			$result = "Query kosong";
+		}
+		return $result;
 	}
 	
 	public function getAPUVCisco() {
@@ -73,29 +91,12 @@ class Oracledb extends CI_Model {
 		
 		$result = array();
 		$index = 0;
-		foreach ($query->result_array () as $row){
-			$result[$index++] = $row;
-		} 
-		return $result;
-		
-	}
-	
-	public function getAPUVAutelan() {
-		$this->oracle = $this->load->database("oracle", TRUE);
-		
-		$query = $this->oracle->query("SELECT b.witel,a.* FROM wifi_nms_ap_detail a, wifi_site_predefined b
-					WHERE b.periode in (SELECT max(periode) FROM wifi_site_predefined) AND a.loc_id=b.loc_id AND 
-						a.under_verify=1 AND b.divre='DIVRE IV' AND a.mac_address in
-						(SELECT mac_address FROM wifi_nms_ap_master_otl WHERE status='Up')
-					order by 1"
-				);
-		$result = array();
-		$index = 0;
-		$cntDown = 0;
-		if($query->num_rows() > 0){
-			$this->db->empty_table ( 'tbl_uvautelan' );
-			
+	if($query->num_rows() > 0){
 			foreach ($query->result () as $row){
+				if($row->JUMLAH_USER == null)
+					$row->JUMLAH_USER = "Not Set";
+				if($row->UP_TIME == null)
+					$row->UP_TIME = "Not Set";
 				if($row->NSR == null)
 					$row->NSR = "Not Set";
 				if($row->BATCH_P == null)
@@ -104,7 +105,6 @@ class Oracledb extends CI_Model {
 					$row->PARTNERSHIP = "Not Set";
 					
 				$temp = array (
-						"id" => $index+1,
 						"witel" => $row->WITEL,
 						"loc_id" => $row->LOC_ID,
 						"ap_name" => $row->AP_NAME,
@@ -138,6 +138,7 @@ class Oracledb extends CI_Model {
 						"nms_source" => $row->NMS_SOURCE,
 						"contr_name" => $row->CONTR_NAME,
 						"p_contr_name" => $row->P_CONTR_NAME,
+						"tipe" => "uvcisco"
 				);
 				// Memasukkan kedalam DB lokal
 				$data [$index ++] = $temp;
@@ -145,9 +146,148 @@ class Oracledb extends CI_Model {
 			$this->db->insert_batch ( 'tbl_apunverif', $data );
 			return $index;
 		}else{
-			$this->db->empty_table ( 'ttbl_apunverif' );
-			return "Query Kosong";
-			
+			return "kosong";
+		}
+		
+	}
+	
+	public function getAPUVAutelan() {
+		$this->oracle = $this->load->database("oracle", TRUE);
+		
+		$query = $this->oracle->query("SELECT b.witel,a.* FROM wifi_nms_ap_detail a, wifi_site_predefined b
+					WHERE b.periode in (SELECT max(periode) FROM wifi_site_predefined) AND a.loc_id=b.loc_id AND 
+						a.under_verify=1 AND b.divre='DIVRE IV' AND a.mac_address in
+						(SELECT mac_address FROM wifi_nms_ap_master_otl WHERE status='Up')
+					order by 1"
+				);
+		$result = array();
+		$index = 0;
+		if($query->num_rows() > 0){
+			foreach ($query->result () as $row){
+				if($row->JUMLAH_USER == null)
+					$row->JUMLAH_USER = "Not Set";
+				if($row->UP_TIME == null)
+					$row->UP_TIME = "Not Set";
+				if($row->NSR == null)
+					$row->NSR = "Not Set";
+				if($row->BATCH_P == null)
+					$row->BATCH_P = "Not Set";
+				if($row->PARTNERSHIP == null)
+					$row->PARTNERSHIP = "Not Set";
+					
+				$temp = array (
+						"witel" => $row->WITEL,
+						"loc_id" => $row->LOC_ID,
+						"ap_name" => $row->AP_NAME,
+						"mac_address" => $row->MAC_ADDRESS,
+						"ap_ip_address" => $row->AP_IP_ADDRESS,
+						"sn" => $row->SN,
+						"location" => $row->LOCATION,
+						"status" => $row->STATUS,
+						"throughput" => $row->THROUGHPUT,
+						"holding_time" => $row->HOLDING_TIME,
+						"jumlah_user" => $row->JUMLAH_USER,
+						"up_time" => $row->UP_TIME,
+						"nsr" => $row->NSR,
+						"propinsi" => $row->PROPINSI,
+						"kota" => $row->KOTA,
+						"user_auth" => $row->USER_AUTH,
+						"user_asoc" => $row->USER_ASOC,
+						"po" => $row->PO,
+						"jml_client" => $row->JML_CLIENT,
+						"regional" => $row->REGIONAL,
+						"bsr" => $row->BSR,
+						"onair_date" => $row->ONAIR_DATE,
+						"divisio" => $row->DIVISIO,
+						"onair_loc" => $row->ONAIR_LOC,
+						"batch_p" => $row->BATCH_P,
+						"segmen1" => $row->SEGMEN1,
+						"segmen2" => $row->SEGMEN2,
+						"jenis" => $row->JENIS,
+						"under_verify" => $row->UNDER_VERIFY,
+						"partnership" => $row->PARTNERSHIP,
+						"nms_source" => $row->NMS_SOURCE,
+						"contr_name" => $row->CONTR_NAME,
+						"p_contr_name" => $row->P_CONTR_NAME,
+						"tipe" => "uvautelan"
+				);
+				// Memasukkan kedalam DB lokal
+				$data [$index ++] = $temp;
+			}
+			$this->db->insert_batch ( 'tbl_apunverif', $data );
+			return $index;
+		}else{
+			return "kosong";
+		}
+	}
+	
+	public function getDivreCisco() {
+		$this->oracle = $this->load->database("oracle", TRUE);
+		
+		$query = $this->oracle->query("select loc_id, ap_name,location,mac_address,ap_ip_address,SN,status,nms_source 
+					from wifi_nms_ap_detail where (nms_source='NMS_JTN06' or nms_source='NMS_YGY11') and status='Up' and witel='WITEL'"
+		);
+		$result = array();
+		$index = 0;
+		if($query->num_rows() > 0){
+			foreach ($query->result () as $row){
+				if($row->LOC_ID == null)
+					$row->LOC_ID = "Not Set";
+				$temp = array(
+						"loc_id" => $row->LOC_ID,
+						"ap_name" => $row->AP_NAME,
+						"location" => $row->LOCATION,
+						"mac_address" => $row->MAC_ADDRESS,
+						"ap_ip_address" => $row->AP_IP_ADDRESS,
+						"sn" => $row->SN,
+						"status" => $row->STATUS,
+						"nms_source" => $row->NMS_SOURCE,
+						"tipe" => "divrecisco"
+				);
+				$data [$index ++] = $temp;
+			}
+			$this->db->insert_batch ( 'tbl_apdivre', $data );
+			return $index;
+		}else{
+			return "kosong";
+		}
+	}
+	
+	public function getDivreAutelan() {
+		$this->oracle = $this->load->database("oracle", TRUE);
+	
+		$query = $this->oracle->query("select loc_id, ap_name,location,mac_address,ap_ip_address,SN,status,nms_source 
+					from wifi_nms_ap_detail where 
+					(p_contr_name='WAC-D4-GBL01' or
+					p_contr_name='WAC-D4-GBL02' or
+					p_contr_name='WAC-D4-GBL03' or
+					p_contr_name='WAC-D4-KBU01' or
+					p_contr_name='WAC-D4-KBU03' or
+					p_contr_name='WAC-D4-KBU02') and status='Up' and witel='WITEL'"
+		);
+		$result = array();
+		$index = 0;
+		if($query->num_rows() > 0){
+			foreach ($query->result () as $row){
+				if($row->LOC_ID == null)
+					$row->LOC_ID = "Not Set";
+				$temp = array(
+						"loc_id" => $row->LOC_ID,
+						"ap_name" => $row->AP_NAME,
+						"location" => $row->LOCATION,
+						"mac_address" => $row->MAC_ADDRESS,
+						"ap_ip_address" => $row->AP_IP_ADDRESS,
+						"sn" => $row->SN,
+						"status" => $row->STATUS,
+						"nms_source" => $row->NMS_SOURCE,
+						"tipe" => "divrecisco"
+				);
+				$data [$index ++] = $temp;
+			}
+			$this->db->insert_batch ( 'tbl_apdivre', $data );
+			return $index;
+		}else{
+			return "kosong";
 		}
 	}
 }
